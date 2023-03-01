0dcd3d0bb8537bdcb39acf0adba9793d5c7b9dd4
Manuel Durand <manuel.durand@mailo.com>
2023-03-01 16:40:17 +0100

    jusqu'a video 6 controller et vue

---
+++ src/Controller/MasterController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MasterController extends AbstractController
+{
+    #[Route('/master', name: 'app_master')]
+    public function index(): Response
+    {
+        return $this->render('master/index.html.twig', [
+            'name' => 'Durand',
+            'firstname' => 'Manuel'
+        ]);
+    }
+}
