42351df5ae76fc1de4f703c1d1afb57b2d763e05
Manuel Durand <manuel.durand@mailo.com>
2023-03-10 14:38:52 +0100

    icone like en ajax ok

---
+++ public/main.js
@@ -0,0 +1,23 @@
+document.querySelectorAll("a.js-like").forEach(function(link){
+    link.addEventListener('click', likeOrUnlike);
+});
+//met à jour le nombre de vues du post-todo et change l'icone, plein quand il est vu et vide sinon
+function likeOrUnlike(event){
+    event.preventDefault();
+    const url = this.href;
+    const spanCount = this.querySelector('span.js-vues');
+    const icone = this.querySelector('i');
+    axios.get(url).then(function(response) {
+        const views = response.data.views;
+        spanCount.textContent = views;
+        if(icone.classList.contains('fas')){
+            icone.classList.replace('fas', 'far')
+        } else {
+            icone.classList.replace('far', 'fas');
+        }
+    })
+
+}
+
+//utiliser 'this' permet de cibler un élément du link ciblé polus haut, donc tout le lien <a>
+// source youtube lior Chamla vidéo AJAX & SYMFONY Initiation
