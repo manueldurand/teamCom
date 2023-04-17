6563bc1717ac42a46405bca5da9563290ec0634d
manuel_durand <manuel.durand@mailo.com>
2023-04-17 13:43:46 +0200

    homeController pour accueil todolist

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController {
+
+    #[Route('/', name: 'liste')]
+    public function index() {
+        return $this->redirectToRoute("todolist");
+    }
+}
